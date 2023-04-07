a985097feefa949c763e03da24a2464294602352
Louis Rouanet <rouanet_l@ohmytag.fr>
2023-04-07 17:41:57 +0200

    clean code

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ const serverUrl =  'http://127.0.0.1:9000/v1'
 
 async function getTodoList() {
     const response = await axios.get(`${serverUrl}/tasks`)
-    console.log(response);
+    // console.log(response);
 }
 
 getTodoList()
--- js/index.js
+++ js/index.js
@@ -46,9 +46,7 @@ function toggleTodoItem(event) {
 
 // Fonction pour afficher toutes les tâches de la liste
 function displayTodoItems() {
-    console.log(axios)
-
-    
+    // console.log(axios)
 
     list.innerHTML = '';
     todoList.forEach(item => {
@@ -82,44 +80,3 @@ form.addEventListener('submit', addTodoItem);
 
 // Appel de la fonction pour afficher les éléments initialement
 displayTodoItems();
-
-// function todoList() {
-//   const todoForm = document.getElementById('todo-form')
-//   const todoInput = document.getElementById('todo-input')
-//   const todoList = document.getElementById('todo-list')
-
-//   // Ajouter un événement pour le formulaire d'ajout de tâche
-//   todoForm.addEventListener('submit', function (event) {
-//     // Empêcher le formulaire de se soumettre
-//     event.preventDefault()
-
-//     // Récupérer la valeur de l'input
-//     const todoText = todoInput.value
-
-//     // Créer un nouvel élément li pour la tâche
-//     const todoItem = document.createElement('li')
-//     todoItem.textContent = todoText
-
-//     // Ajouter un bouton pour supprimer la tâche
-//     const deleteButton = document.createElement('button')
-//     deleteButton.textContent = 'Delete'
-//     deleteButton.classList.add('btn', 'btn-danger', 'btn-sm', 'float-right')
-//     todoItem.appendChild(deleteButton)
-
-//     // Ajouter un événement pour le bouton de suppression
-//     deleteButton.addEventListener('click', function () {
-//       todoItem.remove()
-//     })
-
-//     // Ajouter un événement pour cocher/décocher la tâche
-//     todoItem.addEventListener('click', function () {
-//       todoItem.classList.toggle('completed')
-//     })
-
-//     // Ajouter la tâche à la liste
-//     todoList.appendChild(todoItem)
-
-//     // Réinitialiser l'input
-//     todoInput.value = ''
-//   })
-// }
